26c63e714528bf11df5b6e2484de98cc5cd30c84
Alex Gomes <alex.gsone@gmail.com>
2024-09-12 18:35:30 -0300

    Exercício: contagem de palavras

--- src/com/contagem/palavrasmap/ContagemPalavras.java
+++ src/com/contagem/palavrasmap/ContagemPalavras.java
@@ -5,10 +5,58 @@
  */
 package com.contagem.palavrasmap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author alex
  */
 public class ContagemPalavras {
-    
+
+    Map<String, Integer> palavraMap;
+
+    public ContagemPalavras() {
+        this.palavraMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, Integer contagem) {
+        palavraMap.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra) {
+        Integer numeroPalavra;
+        numeroPalavra = palavraMap.get(palavra);
+        palavraMap.put(palavra, --numeroPalavra);
+    }
+
+    public void exibirContagemPalavras() {
+        System.out.println(palavraMap);
+    }
+
+    public String encontrarPalavraMaisFrequente() {
+        String palavraMaisFrequente = null;
+        int chavePalavra = 0;
+        for (String p : palavraMap.keySet()) {
+            if (palavraMap.get(p) > chavePalavra) {
+                palavraMaisFrequente = p;
+                chavePalavra = palavraMap.get(p);
+            }
+        }
+        return palavraMaisFrequente;
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras contagemPalavras = new ContagemPalavras();
+        contagemPalavras.adicionarPalavra("CASA", 5);
+        contagemPalavras.adicionarPalavra("livro", 3);
+        contagemPalavras.adicionarPalavra("bike", 2);
+        contagemPalavras.exibirContagemPalavras();
+
+        contagemPalavras.removerPalavra("CASA");
+
+        contagemPalavras.exibirContagemPalavras();
+
+        System.out.println("A palavra mais frequente é: " + contagemPalavras.encontrarPalavraMaisFrequente());
+    }
 }
