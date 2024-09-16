79b8c96ff1893a1395c67655bc7fcf00bd0e5bd8
Alex Gomes <alex.gsone@gmail.com>
2024-09-15 23:11:13 -0300

    LivrariaOnline.java

--- src/com/livraria/online/LivrariaOnline.java
+++ src/com/livraria/online/LivrariaOnline.java
@@ -5,10 +5,52 @@
  */
 package com.livraria.online;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *
  * @author alex
  */
 public class LivrariaOnline {
-    
+
+    Map<String, Livro> livroMap;
+
+    public LivrariaOnline() {
+        this.livroMap = new HashMap<>();
+    }
+
+    public void adicionarLivro(String link, String titulo, String autor, double preco) {
+        livroMap.put(link, new Livro(titulo, autor, preco));
+    }
+
+    public void removerLivro(String titulo) {
+        String chaveRemover = a;
+        for (String l : livroMap.keySet()) {
+            if (l.) {
+//                System.out.println("kk");
+//               livroMap.remove("www.amazon.com/5");
+                
+                System.out.println(livroMap.get(l));
+            }
+        }
+    }
+
+    public Map<String, Livro> exibirLivrosOrdenadosPorPreco() {
+        Map<String, Livro> livrosExibirPorPreco = new TreeMap<>(livroMap);
+        return livrosExibirPorPreco;
+    }
+
+    public static void main(String[] args) {
+        LivrariaOnline livrariaOnline = new LivrariaOnline();
+
+        livrariaOnline.adicionarLivro("www.amazon.com/5", "5 pontos do calvinismo", "Charles Spurgeon", 30.50);
+        livrariaOnline.adicionarLivro("www.amazon.com/sousalvo", "Sou salvo?", "Augustos Nicodemos", 50.50);
+        livrariaOnline.adicionarLivro("www.amazon.com/vocequerumamigo?", "Você quer um Amigo", " J.C. Ryle ", 1.99);
+
+        livrariaOnline.removerLivro("5 pontos do calvinismo");
+        System.out.println(livrariaOnline.exibirLivrosOrdenadosPorPreco());
+
+    }
 }
---
+++ src/com/livraria/online/Livro.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.livraria.online;
+
+/**
+ *
+ * @author alex
+ */
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo=" + titulo + ", autor=" + autor + ", preco=" + preco + '}';
+    }
+
+}
